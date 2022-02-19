0e90b4a1bf2379fdae28df03dc5e262a56937506
wbruns <wbruns07@gmail.com>
2022-02-19 14:17:54 -0600

    installed inquirer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -11,3 +12,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
